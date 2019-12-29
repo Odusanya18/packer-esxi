972db835b9762925b5298ce1509ca2710e26ded4
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2019-12-29 13:29:02 +0100

    changed installs to quiet

--- scripts/kubernetes-tools.sh
+++ scripts/kubernetes-tools.sh
@@ -2,14 +2,14 @@
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/root/bin
 export DEBIAN_FRONTEND=noninteractive
-apt-get install iptables-persistent -y
+apt-get install iptables-persistent -qy
 iptables -F
 iptables-save
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -qy
 mkdir -p /etc/docker
 cat > /etc/docker/daemon.json <<EOF
 {
-  "exec-opts": ["native.cgroupdriver=systemd"]
+  "exec-opts": ["native.cgroupdriver=systemd"],
   "insecure-registries" : [ "10.0.0.0/8:5000" ]
 }
 EOF
@@ -25,7 +25,6 @@ cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt install docker-ce=18.06.3~ce~3-0~debian kubelet=1.17.0-00 kubeadm=1.17.0-00 kubectl=1.17.0-00 -y
+apt install docker-ce=18.06.3~ce~3-0~debian kubelet=1.17.0-00 kubeadm=1.17.0-00 kubectl=1.17.0-00 -qy
 apt-mark hold kubelet kubeadm kubectl docker-ce
-systemctl enable docker && systemctl start docker
-systemctl enable kubelet && systemctl start kubelet
+systemctl enable kubelet && systemctl enable docker
