6befc91a017ecfef600e8a62728d3c95e06e42c6
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2019-12-29 20:32:23 +0100

    changed from '_'

--- scripts/kubernetes-images.sh
+++ scripts/kubernetes-images.sh
@@ -5,10 +5,10 @@ flannel_release_tag="v0.11.0-arm64"
 
 ## Pre-fetch Kubernetes release image, so that `kubeadm init` is a bit quicker
 images=(
-  "gcr.io/google_containers/kube-proxy-amd64:${kubernetes_release_tag}"
-  "gcr.io/google_containers/kube-apiserver-amd64:${kubernetes_release_tag}"
-  "gcr.io/google_containers/kube-scheduler-amd64:${kubernetes_release_tag}"
-  "gcr.io/google_containers/kube-controller-manager-amd64:${kubernetes_release_tag}"
+  "gcr.io/google-containers/kube-proxy-amd64:${kubernetes_release_tag}"
+  "gcr.io/google-containers/kube-apiserver-amd64:${kubernetes_release_tag}"
+  "gcr.io/google-containers/kube-scheduler-amd64:${kubernetes_release_tag}"
+  "gcr.io/google-containers/kube-controller-manager-amd64:${kubernetes_release_tag}"
   "gcr.io/google-containers/etcd-amd64:3.4.3"
   "gcr.io/google-containers/pause-amd64:3.1"
   "gcr.io/google-containers/coredns:1.6.6"
